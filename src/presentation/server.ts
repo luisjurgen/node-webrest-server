04294de35c8dc3d9fb940099ba9f902108d35d15
Luis Us Azurdia <luisjurgenus@gmail.com>
2024-12-15 16:17:48 -0600

    crud implemented

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options{
    port:number,
-   public_path?:string
+   public_path?:string,
+   routes:Router
 }
 
 export class Server{
@@ -12,23 +13,31 @@ export class Server{
 
    private readonly port: number;
    private readonly publicPath: string;
+   private readonly routes:Router;
 
    constructor(options:Options){
-      const {port,public_path='public'} = options;
+      const {port,routes,public_path='public'} = options;
 
       this.port= port;
       this.publicPath=public_path
+      this.routes = routes;
 
    }
 
    async start(){
       
-      //*middleware
+      //*Middleware //el .use es para un middleware
+      this.app.use(express.json()) //RAW: parsea el objeto que viene en el body y lo transforme en un objeto JSON
+      this.app.use(express.urlencoded({extended:true}))//XML-URLENCODED: parse el objeto que viene en el body y lo transforma en un objeto URLENCODED
 
       //* Public Folder
-
       this.app.use(express.static(this.publicPath))
 
+      //*REST 
+      this.app.use(this.routes);
+
+
+         //*SPA
       this.app.get('*',(req,res)=>{
          const indexPath = path.join(__dirname+`../../../${this.publicPath}/index.html`);
          res.sendFile(indexPath);
